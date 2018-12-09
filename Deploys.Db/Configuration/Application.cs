1c8aaa28b9fb15740eeed74b9383e2a7a4a8dc3a
wakuwaku3 <takushi_119@live.jp>
2018-12-09 14:19:30 +0900

    不要なコードを削除

--- Deploys.Db/Configuration/Application.cs
+++ Deploys.Db/Configuration/Application.cs
@@ -1,36 +1,24 @@
 using System;
-using System.IO;
-using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.CommandLineUtils;
 using Microsoft.Extensions.Logging;
-using Tmpps.Infrastructure.Common.IO.Interfaces;
 using Tmpps.Infrastructure.Data.Migration.Interfaces;
 
 namespace Deploys.Db.Configuration
 {
     public class Application
     {
-        private CancellationTokenSource cancellationTokenSource;
         private CommandLineApplication application;
         private ILogger logger;
-        private IMigrationConfig config;
-        private IPathResolver pathResolver;
         private IMigrationUseCase migrationUseCase;
 
         public Application(
             CommandLineApplication application,
-            CancellationTokenSource cancellationTokenSource,
             ILogger logger,
-            IMigrationConfig config,
-            IPathResolver pathResolver,
             IMigrationUseCase migrationUseCase)
         {
             this.application = application;
-            this.cancellationTokenSource = cancellationTokenSource;
             this.logger = logger;
-            this.config = config;
-            this.pathResolver = pathResolver;
             this.migrationUseCase = migrationUseCase;
         }
 
@@ -43,10 +31,6 @@ namespace Deploys.Db.Configuration
             {
                 return await this.ExecuteOnErrorHandleAsync("migration", async() =>
                 {
-                    var dir = this.pathResolver.ResolveDirectoryPath(this.config.Path);
-                    var path = Path.Combine(Directory.GetCurrentDirectory(), this.config.Path);
-                    var fullpath = Path.GetFullPath(path);
-
                     return await this.migrationUseCase.ExecuteAsync();
                 });
             });
