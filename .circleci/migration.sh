45c59a01cd85632a6c05af90df3eeb77a0b8dd32
wakuwaku3 <takushi_119@live.jp>
2018-12-16 16:02:27 +0900

    修正

--- .circleci/migration.sh
+++ .circleci/migration.sh
@@ -1,4 +1,4 @@
-result=$(curl -X POST 127.0.0.1:80/migrations -H "Content-Length:0")
+result=$(curl -X POST 127.0.0.1:80/migration -H "Content-Length:0")
 echo $result
 if [ $result -eq '0' ]; then
   echo '成功'
